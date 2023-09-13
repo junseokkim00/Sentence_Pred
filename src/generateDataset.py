46660f0727d34911518240cc2cf1ac1737de67df
junseok Kim <junseokkim00@berkeley.edu>
2023-09-13 22:28:14 +0900

    Preprocess data and generate dataset for gpt-2

--- src/generateDataset.py
+++ src/generateDataset.py
@@ -10,12 +10,20 @@ with open(file_path) as file:
 data_list = [i.rstrip() for i in data_list]
 inputs = []
 output = []
+notContained=0
 for txt in data_list:
     txt_list = txt.split()
-    conjunction_idx = txt_list.index('그런데')
+    try:
+        conjunction_idx = txt_list.index('그런데')
+    except:
+        notContained+=1
+        continue
     inputs.append(' '.join(txt_list[:conjunction_idx+1]))
     output.append(' '.join(txt_list[conjunction_idx+1:]))
 
+print(f'len(data_list): {len(data_list)}')
+print(f'notContained: {notContained}')
+
 d = {'prev': inputs, 'next': output}
 df = pd.DataFrame(data=d)
-df.to_csv('train.csv')
+df.to_csv('./data/train.csv')
